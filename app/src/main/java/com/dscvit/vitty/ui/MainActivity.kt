5f865100359d3132eaa095325c7cf2821216c400
iamyajat <myajat21@gmail.com>
2021-03-25 14:00:42 +0530

    chore: initial app added

---
+++ app/src/main/java/com/dscvit/vitty/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dscvit.vitty.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dscvit.vitty.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
