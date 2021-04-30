291e9bd6a3219420745887976bd1e0ff1fbac877
iamyajat <myajat21@gmail.com>
2021-04-30 16:06:16 +0530

    feat: initial notification system added

---
+++ app/src/main/java/com/dscvit/vitty/notif/AlarmReceiver.kt
@@ -0,0 +1,26 @@
+package com.dscvit.vitty.notif
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver : BroadcastReceiver() {
+    var i = 0;
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+        if (context != null) {
+            NotificationHelper.sendNotification(
+                context,
+                "Cool Notif Title",
+                "Sekrt Msg",
+                "WOW this is such a cool notification. I love it! WOWOW Mazza aa gaya!",
+                "Notify",
+                i++
+            )
+        }
+
+//        val mediaPlayer: MediaPlayer = MediaPlayer.create(context, R.raw.alarm)
+//        mediaPlayer.isLooping = false
+//        mediaPlayer.start()
+    }
+}
---
+++ app/src/main/java/com/dscvit/vitty/notif/NotificationHelper.kt
@@ -0,0 +1,60 @@
+package com.dscvit.vitty.notif
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.dscvit.vitty.R
+import com.dscvit.vitty.ui.schedule.ScheduleActivity
+
+
+object NotificationHelper {
+    fun createNotificationChannel(
+        context: Context,
+        name: String,
+        descriptionText: String,
+    ) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "${context.packageName}-$name"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    fun sendNotification(
+        context: Context,
+        title: String,
+        text: String,
+        bigText: String,
+        channelName: String,
+        notificationId: Int,
+    ) {
+        val channelId = "${context.packageName}-$channelName"
+        val intent = Intent(context, ScheduleActivity::class.java)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+        val builder = NotificationCompat.Builder(context, channelId)
+            .setSmallIcon(R.drawable.ic_pizza)
+            .setContentTitle(title)
+            .setContentText(text)
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText(bigText)
+            )
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(context)) {
+            notify(notificationId, builder.build())
+        }
+    }
+}
