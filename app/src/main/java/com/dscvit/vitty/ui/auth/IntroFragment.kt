9f48ee619ef7f0b5186d359a9b5b6f152c745fb6
iamyajat <myajat21@gmail.com>
2021-05-26 14:54:30 +0530

    feat: intro taglines added

--- app/src/main/java/com/dscvit/vitty/ui/auth/IntroFragment.kt
+++ app/src/main/java/com/dscvit/vitty/ui/auth/IntroFragment.kt
@@ -4,19 +4,48 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.dscvit.vitty.R
+import com.dscvit.vitty.databinding.FragmentIntroBinding
 
 class IntroFragment : Fragment() {
+
+    private lateinit var binding: FragmentIntroBinding
+    private var fragID = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val root = inflater.inflate(R.layout.fragment_intro, container, false)
-        val fragID = requireArguments().getString("frag_id")?.toInt()
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_intro,
+            container,
+            false
+        )
+        fragID = requireArguments().getString("frag_id")?.toInt()!!
+        setupPager()
+        return binding.root
+    }
 
-        return root
+    private fun setupPager() {
+        binding.apply {
+            when (fragID) {
+                0 -> {
+                    heading.text = requireContext().getString(R.string.intro1_title)
+                    subHeading.text = requireContext().getString(R.string.intro1_subtitle)
+                }
+                1 -> {
+                    heading.text = requireContext().getString(R.string.intro2_title)
+                    subHeading.text = requireContext().getString(R.string.intro2_subtitle)
+                }
+                2 -> {
+                    heading.text = requireContext().getString(R.string.intro3_title)
+                    subHeading.text = requireContext().getString(R.string.intro3_subtitle)
+                }
+            }
+        }
     }
 }
